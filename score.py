607286bb9d440347dfe97bb3a1fa3bd56c806aae
nithins1 <nithin@singhalhome.org>
2021-12-15 13:20:56 -0800

    Adding comments, minor improvements

--- score.py
+++ score.py
@@ -3,12 +3,14 @@ import os
 import argparse
 
 has_termcolor = True
+
+# termcolor is recommended but not necessary
 try:
     from termcolor import colored
 except ImportError:
     has_termcolor = False
 
-def input_int(prompt, max_val=-1):
+def input_int(prompt, max_val=None):
     """
     Gets user input, retrying if the input is not an integer.
 
@@ -21,20 +23,20 @@ def input_int(prompt, max_val=-1):
     """
     while True:
         in_str = input(prompt)
-        if in_str.isnumeric() and max_val != -1 and int(in_str) <= max_val:
+        if in_str.isnumeric() and max_val is not None and int(in_str) <= max_val:
             return int(in_str)
-        else:
+        else: # Retry query but set prompt text to red
             if has_termcolor:
                 prompt = colored(prompt, "red", attrs=['bold'])
 
 if __name__ == "__main__":
-    LEFT_MARGIN = 12
+    LEFT_MARGIN = 12 # Size of left margin for displaying round number in table
 
     parser = argparse.ArgumentParser(description="Creates score card for a wizard game.")
     parser.add_argument("-v", "--verbose", action="store_true", help="print round number before each row in table")
     parser.add_argument("-n", "--nodisplay", action="store_true", help="only display table at the end of the game")
-    parser.add_argument("-b", "--base", type=int, default=10, help="base points awarded for bidding correctly")
-    parser.add_argument("-p", "--per", type=int, default=10, help="points gained/lost per bid")
+    parser.add_argument("-b", "--base", type=int, default=10, metavar="<points>", help="base points awarded for bidding correctly")
+    parser.add_argument("-p", "--per", type=int, default=10, metavar="<points>", help="points gained/lost per bid")
     args = parser.parse_args()
 
     names = input("Enter names (sep. by spaces): ").split()
@@ -60,13 +62,16 @@ if __name__ == "__main__":
     while total_cards <= 60:
         print(f"ROUND {round_num}".center(width, '-'))
 
-        bids = {}
-        wins = {}
-        for n in names:
-            bids[n] = input_int(f"{n} bid: ", round_num)
+        bids = {} # Bid for each player in the current round
+        wins = {} # Number of tricks won in the current round
 
+        # Query for each player's bid number
         for n in names:
-            wins[n] = input_int(f"{n} wins: ", round_num)
+            bids[n] = input_int(f"{n}'s bid: ", round_num)
+
+        # Query for each player's win number
+        for n in names:
+            wins[n] = input_int(f"{n}'s wins: ", round_num)
 
         if args.verbose:
             table += f"Round {round_num}:".ljust(LEFT_MARGIN)
@@ -78,17 +83,24 @@ if __name__ == "__main__":
 
         if args.verbose:
             table += " " * LEFT_MARGIN
+
+        # Compute current score for every player
         for n in names:
             if bids[n] == wins[n]:
                 score[n] += args.base + wins[n] * args.per
             else:
                 score[n] -= abs(wins[n] - bids[n]) * args.per
+
+            # Add scores to table
             table += "|" + str(score[n]).center(cell_size)
+
         table += "|\n"
         table += row_break
         if not args.nodisplay:
             print(table)
         round_num += 1
         total_cards += len(names)
+
+    # Print final state of board
     if args.nodisplay:
         print(table)
